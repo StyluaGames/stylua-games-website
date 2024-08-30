991d84ee995192bf8b61e43a0d3e064ecc164e47
Dmitry <103138302+nuafirytiasewo@users.noreply.github.com>
2024-08-31 02:40:16 +0400

    pre-alpha 0.1 2024

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-dark">
+    <AppNavbar />
+    <div class="container">
+      <iframe 
+        src="https://store.steampowered.com/widget/1737640/" 
+        frameborder="0" 
+        width="646" 
+        height="190">
+      </iframe>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavbar from './components/AppNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppNavbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "~bootstrap/dist/css/bootstrap.min.css";
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+.bg-dark {
+  background-color: #343a40;
 }
 </style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-xxl">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/Stylua.png" alt="" width="195,68" height="79,68" />
+        </a>
+        <button 
+          class="navbar-toggler" 
+          type="button" 
+          @click="isNavCollapsed = !isNavCollapsed" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div :class="['collapse', 'navbar-collapse', { show: !isNavCollapsed }]" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Наши игры</a>
+            </li>
+            <li class="nav-item" v-for="link in links" :key="link.name">
+              <a class="nav-link" :href="link.url">{{ link.name }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+  data() {
+    return {
+      isNavCollapsed: true,
+      links: [
+        { name: 'Мы в YouTube', url: '#' },
+        { name: 'Мы в TikTok', url: '#' },
+        { name: 'Мы в Instagram', url: '#' },
+        { name: 'Мы в Twitter', url: '#' },
+        { name: 'Мы в Facebook', url: '#' },
+        { name: 'Мы в ВК', url: '#' },
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.navbar-brand img {
+  width: 195.68px;
+  height: 79.68px;
+}
+</style>
